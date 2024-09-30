68b6a1c00544ac047c6085af95a1e6a58390277e
Webjaoc <Webjaoc>
2024-09-30 17:10:15 +0200

    estado3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "",
+  base: "https://webjaoc.github.io/ta-te-ti",
 })
 //https://webjaoc.github.io/ta-te-ti
